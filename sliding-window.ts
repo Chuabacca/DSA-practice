62ce15ddfadee250743e53c36bbb27c946c0a7b2
Chuabacca <jchua.yf@gmail.com>
2025-09-11 18:45:22 -0700

    sliding window medium

--- sliding-window.ts
+++ sliding-window.ts
@@ -43,3 +43,98 @@ const sumOfThrees = (arr: number[], k: number) => {
 // we are able to achieve O(n).
 
 sumOfThrees(case1, 3)
+
+// ========================
+
+// Medium – Smallest substring containing all characters of another string
+// Example: Given "ADOBECODEBANC" and target "ABC", find the smallest substring that
+// contains all A, B, and C.
+//
+//Why sliding window? You’re “stretching” the right pointer to include enough characters,
+//then “shrinking” the left pointer to minimize the window while still meeting the requirement.
+
+const title2 = '===SMALLEST SUBSTRING==='
+const case2 = "ADOBECODEBANC"
+const target2 = "ABC"
+const expected2 = "BANC"
+
+const smallestSubstring = (str: string, tar: string) => {
+  console.log(title2)
+  // Creat a dictionary of the values we need and tracking variables
+  let need: {[key: string]: number} = {}
+  
+  for (const c of tar) {
+    need[c] = (need[c] ?? 0) + 1
+  }
+
+  // Create tracking variables
+  // The number of required characters we've collected so far
+  let have: {[key: string]: number} = {}
+  // Efficiently track whether or not we have a valid substring
+  let haveCount = 0
+  let requiredCount = Object.keys(need).length
+  // pointers
+  let left = 0
+  let right = 0
+  // track the meinimum substring
+  let minStart = 0
+  let minLength = Infinity
+
+  while (right < str.length) {
+    // Advance the right pointer
+    const c = str[right]
+    if (c) {
+      have[c] = (have[c] ?? 0) + 1
+      if (have[c] === need[c]) {
+        haveCount++
+      }
+    }
+    right++
+
+    // When we find all the required characters, try to advance the left pointer and
+    // close the window to find a smaller valid substring
+    while (haveCount === requiredCount) {
+      // If the current substring is smaller, update the min start and length
+      if ((right - left) < minLength) {
+        minStart = left
+        minLength = right - left
+      }
+
+      // Remove a character on the left and recheck requirements
+      const d = str[left]
+      if (d) {
+        // If the character on the left is one that we need, decrement the value in have
+        if (d in need) {
+          have[d] = (have[d] ?? 0) - 1
+          // If we are below the minimum characters we need, decrement that haveCount tracking
+          if (have[d] < need[d]!) {
+            haveCount--
+          }
+        }
+      }
+      left++
+    }
+  }
+
+  console.log(`Expected: ${expected2}`)
+  console.log(`Result:   ${str.slice(minStart, minStart + minLength)}`)
+}
+
+// Takeaways:
+// * Use dictionaries to efficiently store and lookup the required characters.
+// * A tracking variables like haveCount and requiredCount let you check requirements
+//   very quickly.
+// * The window pointers always move, track the best match for the requirements with
+//   separate vars.
+// * Nesting loops in a sliding window solution is OK because only one loop runs at a
+//   time.
+
+smallestSubstring(case2, target2)
+
+// ========================
+
+// Hard – Longest substring with at most k distinct characters
+// Example: Given "eceba" and k = 2, the answer is "ece" with length 3.
+// 
+// Why sliding window? The right pointer expands to add new characters, and the left
+// pointer contracts when you exceed k distinct characters, always maintaining the invariant.
