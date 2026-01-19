09873024cb9480028380fd6a4c4e501078c8017e
Chuabacca <jchua.yf@gmail.com>
2026-01-19 15:41:49 -0800

    add non-descending-array problem

---
+++ non-descending-array.ts
@@ -0,0 +1,29 @@
+/**
+ * Given an array of integers which are all 1, 2, or 3,
+ * return the minimum number of removals from the input array
+ * to achieve a non-descending array.
+ * 
+ * A non-descending array is one where each element is greater than
+ * or equal to the previous element: arr[i] <= arr[i+1]
+ * 
+ * Example:
+ * Input: [1, 3, 2, 1, 3]
+ * Output: 2
+ * Explanation: Remove indices 1 and 3 to get [1, 2, 3]
+ * 
+ * Example:
+ * Input: [3, 2, 1]
+ * Output: 2
+ * Explanation: Remove any 2 elements to get a single element (always non-descending)
+ * 
+ * Example:
+ * Input: [1, 1, 2, 2, 3, 3]
+ * Output: 0
+ * Explanation: Already non-descending
+ */
+function minRemovalsForNonDescending(arr: number[]): number {
+  // TODO: implement solution
+  return 0;
+}
+
+export { minRemovalsForNonDescending };
